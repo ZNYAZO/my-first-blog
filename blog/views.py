e42e3356bcc830b75f02d2ce875b64f23bca0ede
ZNYAZO <znyazo@gmail.com>
2018-04-10 09:52:37 +0200

    We added - publish, edit and delete buttons to our blog posts.

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,6 @@ def post_new(request): # View for the form
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)# after clicking "save" button, you are redirected to
                                                       #  "post_detail" template
@@ -37,9 +36,24 @@ def post_edit(request, pk): # View for editing an already running Blog Post
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request): # View to display drafts/unpublished blog Posts
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk): # View to publish drafts/unpublished blog Posts
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk): # View to remove/delete blog Posts
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list') # gets redirected to 'post_list'view after deleting the post
+                                 # so as to get further instructions there

e0d7e87a75e9760af8b420c3d7c42db9bf80bca5
ZNYAZO <znyazo@gmail.com>
2018-04-15 13:42:03 +0200

    Adding and managing Comments.

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
 
+# form to create new Blog Post
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+#Let your readers write comments
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
